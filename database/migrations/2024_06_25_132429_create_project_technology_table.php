854975ae03aa66e8d3380b61d1fb09148f5afa7c
andrea-bertaglia <andreabertaglia@gmail.com>
2024-06-25 15:31:17 +0200

    feat: create pivot project_technology table

---
+++ database/migrations/2024_06_25_132429_create_project_technology_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->references('id')->on('technologies')->cascadeOnDelete();
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects')->cascadeOnDelete();
+            $table->primary(['technology_id', 'project_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};
